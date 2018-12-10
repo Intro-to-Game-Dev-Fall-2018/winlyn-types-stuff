0b3bda16643f545e0bec62bb6ca4430caa17fc3d
Winlyn Liu <wl1698@nyu.edu>
2018-12-10 06:30:30 -0500

    fixed some of the text/button formatting, added player sprites and background music

--- Kimmy/Assets/Plugins/Ink/Example/Scripts/BasicInkExample.cs
+++ Kimmy/Assets/Plugins/Ink/Example/Scripts/BasicInkExample.cs
@@ -8,19 +8,26 @@ public class BasicInkExample : MonoBehaviour
 {
 
 	public AudioSource click;
+	public Sprite dana;
+	public Sprite kimmy;
+	public Sprite danaMom;
+	public Sprite kimmyMom;
+	public Sprite dean;
+	public Sprite donna;
+	public Sprite janey;
+	public Sprite jimmy;
 	
 	[SerializeField] private TextAsset _inkJsonAsset;
 	[SerializeField] private Story story;
 	
 	// UI Prefabs
-	[SerializeField]
-	private Text textPrefab;
-	[SerializeField]
-	private Button buttonPrefab;
+	[SerializeField] private Text textPrefab;
+	[SerializeField] private Button buttonPrefab;
 	
 	
-	[SerializeField]
-	private Canvas canvas;
+	[SerializeField] private Canvas canvas;
+
+	[SerializeField] private Image imagePrefab;
 
 	// Remove the default 
 	// Creates a new Story object with the compiled story which we can then play!
@@ -29,7 +36,6 @@ public class BasicInkExample : MonoBehaviour
 	{
 		story = new Story(_inkJsonAsset.text);
 		RemoveChildren();
-		if (!story.canContinue) return;
 		var text = story.Continue();
 		if (story.currentChoices.Count > 0)
 		{
@@ -60,8 +66,6 @@ public class BasicInkExample : MonoBehaviour
 		{
 			for (var i = 0; i < story.currentChoices.Count; i++)
 			{
-				//KeyCode.Alpha1 = 49
-				// "(KeyCode)49" casts 49 to the KeyCode enum
 				if (Input.GetKeyDown((KeyCode) 49 + i))
 				{
 					story.ChooseChoiceIndex(i);
@@ -101,32 +105,69 @@ public class BasicInkExample : MonoBehaviour
 			for (var i = 0; i < story.currentChoices.Count; i++) {
 				Choice choices = story.currentChoices [i];
 				Button button = CreateChoiceView (choices.text.Trim ());
-				// Tell the button what to do when we press it
 				button.onClick.AddListener (delegate {
 					OnClickChoiceButton (choices);
 				});
 			}
 		}
 		CreateContentView(text);
-		
-		//RefreshView();
-		
-	}
-
-	void CreateContentView(string text)
-	{
-		Text storyText = Instantiate(textPrefab) as Text;
-		storyText.text = text;
-		storyText.transform.SetParent(canvas.transform, false);
 	}
 	
-	void RemoveChildren () {
-		int childCount = canvas.transform.childCount;
-		for (int i = childCount - 1; i >= 0; --i) {
-			GameObject.Destroy (canvas.transform.GetChild (i).gameObject);
+	void CreateContentView(string text)
+	{
+		var storyText = Instantiate(textPrefab);
+		var storyImage = Instantiate(imagePrefab);
+		storyText.text = text;
+		storyText.transform.SetParent (canvas.transform, false);
+		storyImage.transform.SetParent (canvas.transform, false);
+
+
+		storyText.text = text;
+		{
+			if (text.Contains("Dana:"))
+			{
+				Debug.Log("dana");
+				storyImage.sprite = dana;
+			}
+			if (text.Contains("Kimmy:"))
+			{
+				Debug.Log("kimmy");
+				storyImage.sprite = kimmy;
+			}
+			if (text.Contains("Mom:"))
+			{
+				Debug.Log("danaMom");
+				storyImage.sprite = danaMom;
+			}
+			if (text.Contains("Mrs. Munro:"))
+			{
+				Debug.Log("kimmyMom");
+				storyImage.sprite = kimmyMom;
+			}
+			if (text.Contains("Dean:"))
+			{
+				Debug.Log("dean");
+				storyImage.sprite = dean;
+			}
+			if (text.Contains("Donna:"))
+			{
+				Debug.Log("donna");
+				storyImage.sprite = donna;
+			}
+			if (text.Contains("Janey:"))
+			{
+				Debug.Log("janey");
+				storyImage.sprite = janey;
+			}
+			if (text.Contains("Jimmy:"))
+			{
+				Debug.Log("jimmy");
+				storyImage.sprite = jimmy;
+			}
 		}
 	}
 	
+	    // Creates a button showing the choice text
 	Button CreateChoiceView (string text) {
 		// Creates the button from a prefab
 		Button choice = Instantiate (buttonPrefab) as Button;
@@ -139,7 +180,14 @@ public class BasicInkExample : MonoBehaviour
 		// Make the button expand to fit the text
 		HorizontalLayoutGroup layoutGroup = choice.GetComponent <HorizontalLayoutGroup> ();
 		layoutGroup.childForceExpandHeight = false;
-
+	
 		return choice;
 	}
-}	
+	
+	void RemoveChildren () {
+		int childCount = canvas.transform.childCount;
+		for (int i = childCount - 1; i >= 0; --i) {
+			GameObject.Destroy (canvas.transform.GetChild (i).gameObject);
+		}
+	}
+}
